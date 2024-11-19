9b179be10889931f729709dc0b335aead198cfa5
Arturselklll <junior.selk@hotmail.com>
2024-11-18 21:18:15 -0300

    Atividade03

---
+++ src/Aplicativos_de_Notas.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class Aplicativos_de_Notas {
+    private JPanel AplicativoDeNotas;
+    private JTextField txtInserirNotas;
+    private JButton btnadicionarNota;
+    private JButton btncalcular;
+    private JTextArea txtaListadeNotas;
+    private JLabel lbldigitarnotas;
+    private JLabel lblResultado;
+    private JButton lbllimpar;
+}
